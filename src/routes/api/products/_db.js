e7f991e7367e66af93a28ac3d7352d53aab49cbd
Jeffrey Charters <jeff.charters@gmail.com>
2022-02-26 20:45:26 -0500

    fix annual product

--- src/routes/api/products/_db.js
+++ src/routes/api/products/_db.js
@@ -4,6 +4,12 @@ export const getYearData = async (year) => {
 	const aggregations = await prisma.entry.aggregate({
 		_sum: {
 			hours: true
+		},
+		where: {
+			date: {
+				gte: new Date(`${year}-01-01`),
+				lte: new Date(`${year}-12-31`)
+			}
 		}
 	});
 	return Promise.resolve(aggregations);
